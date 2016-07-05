5c3ca64a4a1456f7eb55c985564d3e40e06bed91
Chris Jensen <chris@goodforus.info>
2016-07-05 17:47:41 +0800

    Add all tests

--- spec/hash_redactor_spec.rb
+++ spec/hash_redactor_spec.rb
@@ -5,7 +5,131 @@ describe HashRedactor do
     expect(HashRedactor::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  let (:subj) { HashRedactor::HashRedactor.new(options) }
+  
+  let (:options) do
+    {
+		redact: redact,
+		encryption_key: 'my really, really, really, secret key'
+	}
+  end
+  
+  let (:data) do
+	{
+		id: 5,
+		email: 'george@example.com',
+		ssn: "George's social security number",
+		address: "#02-03 Big Building, 22nd St, NY"
+	}
+  end
+
+  let (:redact) do
+	{
+		:email => :digest,
+		:ssn => :remove,
+		:address => :encrypt
+	}
+  end
+
+  def subhash(hash, *extract)
+    hash.select{|key, value| extract.include?(key) }
+  end
+
+  describe "#redact" do
+	context "incomplete configuration" do
+		it "raises error for no redact hash" do
+			r = HashRedactor::HashRedactor.new()
+			expect do
+				r.redact(data)
+			end.to raise_error("Don't know what to redact. Please configure the redact hash when initializing or pass as an argument to redact.")
+		end
+	
+		it "raises error if no encryption key given" do
+			r = HashRedactor::HashRedactor.new(redact: redact)
+			expect do
+				r.redact(data)
+			end.to raise_error("No encryption key specified. Please pass :encryption_key in options to new or redact")
+		end
+	end
+	
+    it "removes data" do
+		result = subj.redact(data, redact: subhash(redact, :ssn))
+		expect(result).not_to have_key(:ssn)
+    end
+    
+    it "digests data" do
+		result = subj.redact(data, redact: subhash(redact, :email))
+		expect(result[:email_digest]).not_to eq(nil)
+		expect(result[:email_digest]).not_to eq(data[:email])
+    end
+
+    it "removes plaintext of digested data" do
+		result = subj.redact(data, redact: subhash(redact, :email))
+		expect(result).not_to have_key(:email)
+	end
+    
+    it "encrypts data" do
+		result = subj.redact(data, redact: subhash(redact, :address))
+		expect(result).to have_key(:encrypted_address)
+
+		expect(result[:encrypted_address]).not_to eq(nil)
+		expect(result[:encrypted_address]).not_to eq(data[:address])
+	end
+	
+    it "removes plaintext of encrypted data" do
+		result = subj.redact(data, redact: subhash(redact, :address))
+		expect(result).not_to have_key(:address)
+	end
+    
+	it "raises error on bad operation" do
+		expect do
+			subj.redact(data, redact: { :email => :weird_new_operation })
+		end.to raise_error("redact called with unknown operation on email: weird_new_operation")
+	end
+
+    it "digested data should be comparable" do
+      data2 = { email: 'george@example.com' }
+	  result = subj.redact(data, redact: subhash(redact, :email))
+	  result2 = subj.redact(data2, redact: subhash(redact, :email))
+	  
+	  expect(result[:email_digest]).not_to eq(nil)
+	  expect(result[:email_digest]).to eq(result2[:email_digest])
+    end
+    
+    it "salt should change digest" do
+      data2 = { email: 'george@example.com' }
+	  result = subj.redact(data, redact: subhash(redact, :email),
+	  							 digest_salt: 'saltsaltsalt')
+	  result2 = subj.redact(data2, redact: subhash(redact, :email))
+
+	  expect(result[:email_digest]).not_to eq(nil)
+	  expect(result[:email_digest]).not_to eq(result2[:email_digest])
+    end
+
+  end
+  
+  describe "#decrypt" do
+	context "incomplete configuration" do
+		it "raises error for no redact hash" do
+			r = HashRedactor::HashRedactor.new()
+			expect do
+				r.decrypt(data)
+			end.to raise_error("Don't know what to decrypt. Please configure the redact hash when initializing or pass as an argument to #decrypt.")
+		end
+		
+		it "raises error if no encryption key given" do
+			r = HashRedactor::HashRedactor.new(redact: redact)
+			expect do
+				r.decrypt(data)
+			end.to raise_error("No encryption key specified. Please pass :encryption_key in options to new or decrypt")
+		end
+	end
+
+    it "encrypted data should be decrypted" do
+		crypted = subj.redact(data, redact: subhash(redact, :address))
+    	decrypted = subj.decrypt(crypted, redact: subhash(redact, :address))
+    	
+    	expect(decrypted[:address]).to eq(data[:address])
+    end    
   end
 end
